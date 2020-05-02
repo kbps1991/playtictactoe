3b08a6fe695b58fef79545167c5c6cbaacb2d7f1
Kiran Balaji <kiran.balaji@cerner.com>
2020-05-02 15:26:12 +0530

    Updates

---
+++ precache-manifest.99c36162c9cdffbe9de1dd261afc4fc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e745c00683147053f5e0647bd3e1417f",
+    "url": "/playtictactoe/index.html"
+  },
+  {
+    "revision": "30af20f44a80c48bfacf",
+    "url": "/playtictactoe/static/css/main.89bc5732.chunk.css"
+  },
+  {
+    "revision": "a0b18543522fb8adf7e7",
+    "url": "/playtictactoe/static/js/2.dd077de5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/playtictactoe/static/js/2.dd077de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30af20f44a80c48bfacf",
+    "url": "/playtictactoe/static/js/main.a85b3fea.chunk.js"
+  },
+  {
+    "revision": "8b83d2bca2c6e68ef4fb",
+    "url": "/playtictactoe/static/js/runtime-main.929e5bbc.js"
+  }
+]);
